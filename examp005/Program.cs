5a8ab03be29296ff30bb0484fee4e569dc5a508e
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 02:13:45 +0600

    генерация массива и нахождение количества элементов в отрезке от 10 до 99

---
+++ examp005/Program.cs
@@ -0,0 +1,24 @@
+﻿int size = 123;
+int minValue = 0;
+int maxValue = 999;
+int[] array = new int[size];
+
+int count = 0;
+
+for (int i = 0; i < array.Length; ++i)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+
+
+for (int i = 0; i < array.Length; ++i)
+{
+    if (array[i] >= 10 && array[i] < 100) 
+    {
+        count++;
+    }
+
+}
+Console.WriteLine("количество элементов в отрезке [10,99] = " + count);
