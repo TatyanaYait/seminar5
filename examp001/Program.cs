87c2b4e6d3e0476c593ab7e06baff074d61c8e59
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 01:51:25 +0600

    вывод заранее заданного массива через запятую

---
+++ examp001/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[3];
+arr[0] = 1;
+arr[1] = 5;
+arr[2] = 38;
+
+string concat = MyJoin(",", arr);
+Console.WriteLine(concat);
+
+string MyJoin(string delimiter, int[] array)
+{
+    string res = "";
+    for (int i = 0; i < array.Length - 1; ++i)
+    {
+        res = res + array[i] + delimiter;
+    }
+    res = res + array[array.Length - 1];
+    return res;
+
+Console.WriteLine(res);
+}
