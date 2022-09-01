d8b5186ce4fb8c61022c66e2a5e53876ffada998
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 02:16:48 +0600

    генерация массива2 из произведений пар элементов массива1

---
+++ examp006/Program.cs
@@ -0,0 +1,24 @@
+﻿int size = 7;
+int minValue = 0;
+int maxValue = 9;
+int[] array = new int[size];
+int [] array2 = array.Length%2 == 1? new int[array.Length/2+1] : new int[array.Length/2];
+
+for (int i = 0; i < array.Length; ++i)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+
+for (int i = 0; i < array.Length/2; ++i)
+{
+int pr = array [i]* array[array.Length - (i+1)];
+array2[i]=pr;
+}
+if (array.Length % 2==1)
+{
+    array2[array2.Length -1] = array [array.Length /2];
+}
+Console.WriteLine(string.Join(",", array2));
+Console.WriteLine();
