78b068040c3758315b27acd710943fd3e00047cf
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 15:22:26 +0600

    подсчет суммы элементов массива с позициями нечетных индексов

---
+++ hw036/Program.cs
@@ -0,0 +1,24 @@
+﻿int size = 7;
+int minValue = -9;
+int maxValue = 9;
+int[] array = new int[size];
+
+int sumI = 0;
+
+for (int i = 0; i < array.Length; ++i)
+{
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; ++i)
+{
+    if (i % 2 == 1)
+    {
+        sumI += array[i];
+    }
+}
+
+Console.WriteLine("сумма элементов с нечетных позиций = " + sumI);
+Console.WriteLine();
