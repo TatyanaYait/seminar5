f5f0de5cc8c822d943e3de6d4ad522a5614b3363
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 15:12:11 +0600

    генерация массива и подсчет колличества четных элементов

---
+++ hw034/Program.cs
@@ -0,0 +1,23 @@
+﻿int size = 11;
+int minValue = 100;
+int maxValue = 999;
+int[] array = new int[size];
+
+int count = 0;
+
+for (int i = 0; i < array.Length; ++i) {
+    array[i] = new Random().Next(minValue, maxValue + 1);
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; ++i) { 
+    int ost = array[i] % 2;
+
+    if ( ost == 0)    {
+        count ++;
+    }
+}
+
+Console.WriteLine("количество четных элементов в массиве = " + count);
+Console.WriteLine();
